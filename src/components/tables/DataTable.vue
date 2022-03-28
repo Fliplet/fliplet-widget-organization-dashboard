7526a0864cec8f132328181155f15f501b0a83dd
OleksandrLavrynenko <oleksandr.lav@upplabs.com>
2022-03-28 10:46:37 +0300

    [Refact]

--- src/components/tables/DataTable.vue
+++ src/components/tables/DataTable.vue
@@ -116,7 +116,7 @@ export default {
       }
 
       if (dataCell.type && dataCell.type === 'dynamic') {
-        return $(node).find('p').data('value');
+        return node.dataset.value;
       }
 
       return node.innerText;
